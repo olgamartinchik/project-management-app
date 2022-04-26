5386aa2ba3de020102addc87fc53031e590d16bc
olgamartinchik <sintcerova@mail.ru>
2022-04-26 17:16:19 +0300

    feat: add main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+import { MainComponent } from './modules/main/main.component';
+import { MainModule } from './modules/main/main.module';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule,
+    MainModule],
   providers: [],
   bootstrap: [AppComponent],
 })
