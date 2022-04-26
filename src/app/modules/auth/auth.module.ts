fa05089c9d3bb4837703db5f42c6afd89ac72081
Andrey Burlakov <andrburl2@yandex.ru>
2022-04-26 16:58:13 +0300

    init: create auth-module

---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthPageComponent } from './components/auth-page/auth-page.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
+
+@NgModule({
+  declarations: [AuthPageComponent, LoginFormComponent, SignupFormComponent],
+  imports: [CommonModule, AuthRoutingModule],
+})
+export class AuthModule {}
