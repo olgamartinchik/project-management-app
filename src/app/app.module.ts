ced3e7e565e2b2f3a6c7a54f03c669739e7658f0
olgamartinchik <sintcerova@mail.ru>
2022-04-28 11:32:48 +0300

    fix: move TranslocoRootModule and HttpClientModule from app module in to core module, delete shared module in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { SharedModule } from './modules/shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
-import { TranslocoRootModule } from './modules/transloco-root.module';
 import { CoreModule } from './modules/core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SharedModule,
-    HttpClientModule,
-    TranslocoRootModule,
-    CoreModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, CoreModule],
 
   providers: [],
   bootstrap: [AppComponent],
--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -5,10 +5,11 @@ import { RouterModule } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TranslocoRootModule } from '../transloco-root.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, RouterModule, TranslocoRootModule],
+  imports: [CommonModule, HttpClientModule, RouterModule, TranslocoRootModule],
 })
 export class CoreModule {}
