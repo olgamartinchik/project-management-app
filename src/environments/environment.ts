4ae6951860dbf377edf1963b10820fb348ab0ab9
olgamartinchik <sintcerova@mail.ru>
2022-05-10 19:02:20 +0300

    fix: add new link for backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://guarded-scrubland-15781.herokuapp.com',
+  // apiUrl: 'https://guarded-scrubland-15781.herokuapp.com',
+  apiUrl: 'https://agile-shore-62657.herokuapp.com/',
 };
