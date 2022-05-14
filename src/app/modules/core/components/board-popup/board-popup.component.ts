9fe316ba563e36d1c8061d7fde7927be6f3abfb1
olgamartinchik <sintcerova@mail.ru>
2022-05-14 18:09:28 +0300

    fix: fix board-popup

--- src/app/modules/core/components/board-popup/board-popup.component.ts
+++ src/app/modules/core/components/board-popup/board-popup.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { map, take } from 'rxjs';
+import { Subject, take, takeUntil, tap } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { BoardService } from '../../services/board.service';
 import { ToggleScrollService } from '../../services/toggle-scroll.service';
@@ -10,17 +10,22 @@ import { updateAllBoards } from 'src/app/redux/actions/board.actions';
 import { ErrorMessagesService } from '../../services/error-messages/error-messages.service';
 import { FormMessagesModel } from '../../models/error-messages.services.models';
 import { FORM_ERROR_MESSAGES } from '../../constants/error-messages.constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-board-popup',
   templateUrl: './board-popup.component.html',
   styleUrls: ['./board-popup.component.scss'],
 })
-export class BoardPopupComponent {
+export class BoardPopupComponent implements OnDestroy {
   // public title!: FormControl;
   public messages: FormMessagesModel = FORM_ERROR_MESSAGES;
 
   public boardForm!: FormGroup;
+
+  private newBoard: any;
+
+  private unsubscribe$: Subject<void> = new Subject<void>();
 
   constructor(
     public boardService: BoardService,
@@ -29,6 +34,7 @@ export class BoardPopupComponent {
     private store: Store<IAppState>,
     public errorMessagesService: ErrorMessagesService,
     private fb: FormBuilder,
+    private router: Router,
   ) {
     this.createForm();
   }
@@ -56,13 +62,28 @@ export class BoardPopupComponent {
       .postBoard({ ...this.boardForm?.value })
       .pipe(
         take(1),
-        map(() => {
+        tap(() => {
           this.store.dispatch(updateAllBoards());
         }),
       )
-      .subscribe();
+      .subscribe((board) => {
+        console.log('new board', board);
+        // this.newBoard=board
+
+        this.router.events.pipe(takeUntil(this.unsubscribe$)).subscribe((event) => {
+          console.log('rout', event);
+          // if ((event as NavigationStart).url !== '/main') {
+          //   this.router.navigate([`/board/${board.id}`]);
+          // }
+        });
+      });
 
     this.toggleScrollService.showScroll();
     this.boardForm?.reset();
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
