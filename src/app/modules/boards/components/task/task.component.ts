c467d2d2f934cf85a0dcab0a7626f29e55498d3f
olgamartinchik <sintcerova@mail.ru>
2022-05-15 18:29:37 +0300

    feat: add delete task

--- src/app/modules/boards/components/task/task.component.ts
+++ src/app/modules/boards/components/task/task.component.ts
@@ -1,6 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { take } from 'rxjs';
 import { ITask } from 'src/app/modules/core/models/ITask.model';
 import { ApiService } from 'src/app/modules/core/services/api.service';
+import { ConfirmService } from 'src/app/modules/core/services/confirm.service';
+import { updateAllBoards } from 'src/app/redux/actions/board.actions';
+import { IAppState } from 'src/app/redux/state.model';
 
 import { TaskService } from '../../services/task.service';
 
@@ -19,13 +24,28 @@ export class TaskComponent {
   @Input() public columnId!: string;
 
   // public name:string='vasya'
-  constructor(public taskService: TaskService, public apiService: ApiService) {}
+  constructor(
+    public taskService: TaskService,
+    public apiService: ApiService,
+    private store: Store<IAppState>,
+    private confirmService: ConfirmService,
+  ) {}
 
   public openPopupEditTask(): void {
     this.taskService.isEditTaskPopup$.next(true);
   }
 
-  public deleteTask(event: Event): void {
+  public confirmDeleteTask(event: Event): void {
     event.stopPropagation();
+    this.confirmService.open(this.deleteTask);
   }
+
+  private deleteTask = (): void => {
+    this.apiService
+      .deleteTask(this.boardId, this.columnId, this.task.id!)
+      .pipe(take(1))
+      .subscribe(() => {
+        this.store.dispatch(updateAllBoards());
+      });
+  };
 }
